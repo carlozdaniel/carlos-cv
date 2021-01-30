671523526266fb423f93db9b37fa113ccea49e7b
carlos daniel hernandez rodriguez <a20160383@gmail.com>
2021-01-30 05:03:04 -0600

    add personal information

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,6 +99,7 @@ const mySwiper = new Swiper('.testimonial__container', {
 /*===== GSAP ANIMATION =====*/ 
 gsap.from('.home__img', {opacity: 0, duration: 2, delay:.5, x:60})
 gsap.from('.home__data', {opacity: 0, duration: 2, delay:.8, y:25})
+gsap.from('.home__greeting', {opacity: 0, duration: 2, delay:.8, y:25})
 
 
 
